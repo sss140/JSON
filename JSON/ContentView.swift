330a0579a43e53fd3e7ebb57ba74a40ab256110b
佐藤一成 <kazunarisato0309@gmail.com>
2020-10-11 20:39:45 +0900

    10102040

--- JSON/ContentView.swift
+++ JSON/ContentView.swift
@@ -40,6 +40,7 @@ struct ContentView: View {
                             }
                         }
                     }
+                    
                     /*.onAppear{
                      store.getAll()
                      }*/
