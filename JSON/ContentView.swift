7a659c303505eb18a86509c4b54f826f7f64f7b6
佐藤一成 <kazunarisato0309@gmail.com>
2020-10-11 17:35:54 +0900

    10101730

--- JSON/ContentView.swift
+++ JSON/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                     /*.onAppear{
                      store.getAll()
                      }*/
-                }.navigationTitle("Movies")
+                }.navigationTitle(self.store.navigationTitle)
             }
         }
     }
--- JSON/MovieStore.swift
+++ JSON/MovieStore.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 struct MovieResponse:Decodable{
-    let movies:[Movie]
+    let movies:[Movie]?
+    let totalResults:String?
     let response:String
+    let error:String?
     private enum CodingKeys: String, CodingKey{
         case movies = "Search"
+        case totalResults
         case response = "Response"
+        case error = "Error"
     }
 }
 
@@ -21,45 +25,53 @@ struct Movie: Decodable{
     let imdbID:String
     let title:String
     let poster:String
+    let year:String
+    let type:String
     
     private enum CodingKeys: String, CodingKey{
         case imdbID
         case title = "Title"
         case poster = "Poster"
+        case year = "Year"
+        case type = "Type"
     }
 }
 
 
 class MovieStore:ObservableObject{
     @Published var movies:[Movie]? = [Movie]()
-    
+    @Published var navigationTitle = "Movies"
     func changeSearchString(searchString:String){
         self.getAll(searchString: searchString)
     }
     
     init(){
-        print("init")
         self.getAll()
     }
     
     func getAll(searchString:String = ""){
+        self.navigationTitle = "Requesting..."
         self.movies?.removeAll()
         let urlString:String = "http://www.omdbapi.com/?apikey=dfd99a49" + (searchString == "" ? "":"&s=\(searchString)")
-        print(urlString)
             guard let url = URL(string: urlString) else{
             fatalError("Invalid URL")
         }
+        
         URLSession.shared.dataTask(with: url){data, response,error in
             guard let data = data, error == nil else{
                 return
             }
             let movieResponse = try? JSONDecoder().decode(MovieResponse.self,from:data)
             if let movieResponse = movieResponse{
+                print(movieResponse)
                 DispatchQueue.main.async {
-                    
                     self.movies = movieResponse.movies
+                    if movieResponse.response == "True"{
+                        self.navigationTitle = searchString + "(\(movieResponse.totalResults!))"
+                    }else{
+                        self.navigationTitle = movieResponse.error!
+                    }
                 }
-                
             }
         }.resume()
         
